11d6edcffb3eba15c5c193452afe45285e831571
stakelovelace <github@stakelovelace.io>
2023-12-23 17:21:48 +0000

    debian docker installation fix

--- scripts/koios-lite.sh
+++ scripts/koios-lite.sh
@@ -187,8 +187,20 @@ docker_install() {
             source /etc/os-release
             case "${ID}" in
                 ubuntu|debian)
+            # Add Docker's official GPG key:
+                    sudo apt-get update
+                    sudo apt-get install -y ca-certificates curl gnupg
+                    sudo install -m 0755 -d /etc/apt/keyrings
+                    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+                    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+                    # Add the repository to Apt sources:
+                    echo \
+                    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+                    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
                     gum spin --spinner dot --title "Updating..." -- echo && sudo apt-get update
-                    gum spin --spinner dot --title "Installing Docker..." -- echo && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+                    gum spin --spinner dot --title "Installing Docker..." -- echo && sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
                     ;;
                 fedora|rhel)
                     gum spin --spinner dot --title "Installing Docker..." -- echo && sudo dnf -y install dnf-plugins-core && sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo && sudo dnf install docker-ce docker-ce-cli containerd.io
