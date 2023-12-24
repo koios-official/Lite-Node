7e396f19fe269aeb5ed29ea30c39ddbacfe61fdd
Boris P <boris@zfs.ninja>
2023-12-24 02:07:23 -0700

    Fix docker install for ubuntu
    Docker install was not working in Ubuntu due to hardcoded URL path to debian.

--- scripts/koios-lite.sh
+++ scripts/koios-lite.sh
@@ -191,12 +191,12 @@ docker_install() {
                     sudo apt-get update
                     sudo apt-get install -y ca-certificates curl gnupg
                     sudo install -m 0755 -d /etc/apt/keyrings
-                    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+                    curl -fsSL https://download.docker.com/linux/${ID}/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
                     sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
                     # Add the repository to Apt sources:
                     echo \
-                    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+                    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/${ID} \
                     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
                     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
                     gum spin --spinner dot --title "Updating..." -- echo && sudo apt-get update
