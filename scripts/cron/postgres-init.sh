cc2f67fbc2191d93f65c19460c0da7a1a256bfea
Greg Beresnev <greg.beresnev@yarris.com>
2024-01-11 17:22:35 +1100

    adding more rpc functions and improving robustness of postgres init script and its invocation

--- scripts/cron/postgres-init.sh
+++ scripts/cron/postgres-init.sh
@@ -14,7 +14,7 @@ fi
 DATE_DIFF=$(( $(date +%s) - $(date --date="$(psql -Aqt -h ${POSTGRES_HOST} -c 'select time from block order by id desc limit 1;')" +%s) ))
 echo "Date Diff is $DATE_DIFF"
 
-[[ $DATE_DIFF -lt 3600 ]] || { echo "date difference greater than 1 hour, so exiting for later retry"; exit 1; }
+[[ $DATE_DIFF -lt 7200 ]] || { echo "date difference greater than 7200 seconds, so exiting for later retry"; exit 1; }
 
 echo "Block table seems to be on tip or close enough, current time is `date`"
 
@@ -27,8 +27,11 @@ fi
 echo "IS_POSTGRES_INITIALIZED is ${IS_PG_INIT}"
 
 if [[ $IS_PG_INIT == "f" ]]; then
-  echo "Commencing PG init"
-  /scripts/lib/install_postgres.sh
+  echo "Commencing PG init..."
+  ls -al /scripts/lib/install_postgres.sh
+  /scripts/lib/install_postgres.sh > foo.out 2>&1
+  echo "Done some stuff, curr time is $CURRTIME"
+  cat foo.out
   psql -h ${POSTGRES_HOST} -qb -c "INSERT INTO ${RPC_SCHEMA}.control_table (key, last_value) VALUES ('postgres_init_timestamp','${CURRTIME}');"
   echo "Done postgres init"
 fi
